09c12d0ca4d2a55be9ad81c3c152c306e55943a6
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2024-11-29 15:02:53 +0700

    lab6_1.cpp completed
    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    string number;
+    int even,odd;
+    even = -1;
+    odd = 0;
+    int x,size;
+    do{
+        if (number != "0"){
+            if (x%2 == 0 && number != "0"){
+                even = even+1;
+            }else {
+                odd = odd+1;
+            }
+             cout << "Enter an integer: ";
+            cin >> number;
+            size = number.size();
+            x = number[size-1];
+        }
+    }while (number != "0");
+    
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
